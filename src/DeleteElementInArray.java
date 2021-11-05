63f6ae88d18c928012f6d40910874fdc0336ebb8
taitv1112 <91813628+taitv1112@users.noreply.github.com>
2021-11-05 08:07:09 +0700

    xoa phan tu trong mang

---
+++ src/DeleteElementInArray.java
@@ -0,0 +1,28 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DeleteElementInArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter element need delete: ");
+        int element = sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                for (int j = i; j < (arr.length-1); j++) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("New one is : "+ Arrays.toString(arr));
+        int[] arr1 = new int[arr.length-1];
+        for (int i = 0; i < (arr.length-1); i++) {
+            arr1[i]=arr[i];
+        }
+        System.out.println("New one is : "+ Arrays.toString(arr1));
+
+    }
+}
